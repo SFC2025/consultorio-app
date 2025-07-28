b92944c6f12799b3453e332e1d54e48dbcc2d88b
s.cossu <santiagocossu870@gmail.com>
2025-07-28 19:17:26 -0300

    Integración completa de login por PIN y contexto de profesionales

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Registro from "./Registro";
 import PanelProfesional from "./pages/PanelProfesional";
+import { ProfesionalProvider } from "./context/ProfesionalContexto"; // Importo el Provider
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Registro />} />
-      <Route path="/panelprofesional" element={<PanelProfesional />} />
-    </Routes>
+    <ProfesionalProvider>
+      <Routes>
+        <Route path="/" element={<Registro />} />
+        <Route path="/panelprofesional" element={<PanelProfesional />} />
+      </Routes>
+    </ProfesionalProvider>
   );
 }
 
 export default App;
+
