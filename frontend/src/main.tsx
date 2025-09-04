519f4fd83d8ef79d3d1c8fe966ebfc6a3b2dc249
s.cossu <santiagocossu870@gmail.com>
2025-09-04 12:38:39 -0300

    chore: tipado de ErrorBoundary (children) en main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,27 @@
-// frontend/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-class ErrorBoundary extends React.Component<{}, { hasError: boolean; err?: any }> {
-  constructor(props: {}) {
+type EBProps = { children: React.ReactNode };
+type EBState = { hasError: boolean; err?: any };
+
+class ErrorBoundary extends React.Component<EBProps, EBState> {
+  constructor(props: EBProps) {
     super(props);
     this.state = { hasError: false };
   }
-  static getDerivedStateFromError(err: any) {
+  static getDerivedStateFromError(err: any): EBState {
     return { hasError: true, err };
   }
   componentDidCatch(err: any, info: any) {
     console.error("ErrorBoundary:", err, info);
   }
-  render() {
+  render(): React.ReactNode {
     if (this.state.hasError) {
       return <div style={{ padding: 16 }}>Se rompió el render. Mirá la consola (ErrorBoundary).</div>;
     }
-    return this.props.children as any;
+    return this.props.children; 
   }
 }
 
